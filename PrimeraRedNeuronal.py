ceb0fabb0d9772f50b3d0f2f8c274b3e78ae7017
Colo Ceppone Tiago <colotiago8@gmail.com>
2025-07-19 01:57:03 -0300

    Update PrimeraRedNeuronal.py

--- PrimeraRedNeuronal.py
+++ PrimeraRedNeuronal.py
@@ -1,5 +1,10 @@
 import tensorflow as tf
 import numpy as np
+from decimal import Decimal, ROUND_HALF_UP
+import matplotlib.pyplot as plt
+
+def redondear(n):
+    return int(Decimal(n).quantize(0, rounding=ROUND_HALF_UP))
 
 celsius = np.array([-40, - 10, 0, 8, 15, 22, 38], dtype= float)
 fahrenheit = np.array([-40, 14, 32, 46, 59, 72, 100], dtype = float)
@@ -9,26 +14,24 @@ capa = tf.keras.layers.Dense(units=1, input_shape=[1])
 modelo = tf.keras.Sequential([capa])
 
 modelo.compile(
-    optimizer = tf.keras.optimizers.Adam(0.01),
+    optimizer = tf.keras.optimizers.Adam(0.1),
     loss = 'mean_squared_error'
 )
 
 print('Comenzando Entrenamiento...')
 
-record = modelo.fit(celsius, fahrenheit, epochs = 7000, verbose = False)
+record = modelo.fit(celsius, fahrenheit, epochs = 1000, verbose = False)
 
 print('Modelo Entrenado!')
 
 print('Hagamos una prediccion')
 
-PruebaDePrediccion = modelo.predict(np.array([15]))
+PruebaDePrediccion = modelo.predict(np.array([20]))
 
-print('El resultado es ', str(PruebaDePrediccion),'Fahrenheit')
+print('El resultado es ', redondear(float(PruebaDePrediccion)),'Fahrenheit')
 
-import matplotlib.pyplot as plt
 plt.xlabel('# Epoca')
-plt.ylabel('Magnitud de Perdida')
-plt.plot(record.history['loss'])
 
-print('Variables del Modelo')
-print(capa.get_weights())
+plt.ylabel('Magnitud de Perdida')
+
+plt.plot(record.history['loss'])
